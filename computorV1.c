49021541b726df91f1343c74dd04ba1ed1d96643
rkhelif <rkhelif@student.42.fr>
2024-07-08 16:55:47 +0200

    cotinue computer v1

--- computorV1.c
+++ computorV1.c
@@ -143,13 +143,12 @@ struct values * parseData(char *s){
     int side = 0;
     for (size_t i = 0; s[i] != '\0'; i++){
         if (s[i] == 32){continue;}
-        else if(s[i] == '/' && s[i] == '*' && s[i] == '+' && s[i] == '-'){
-            struct values *tmp = malloc(sizeof(data));
-            char *tmp2 = NULL;
+        else if(s[i] == '/' || s[i] == '*' || s[i] == '+' || s[i] == '-'){
+            struct values *tmp = malloc(sizeof(struct values));
             tmp->side = side;
             if (s[i] == '+'){
-                tmp->sign = 1;
-                tmp->val = strdup("+");
+                   tmp->sign = 1;
+                   tmp->val = strdup("+");
             }
             else if (s[i] == '-'){
                 tmp->sign = 2;
@@ -177,8 +176,8 @@ struct values * parseData(char *s){
                 tmp->next = NULL;
                 tmp->prev = tmpData;
             }
-        }else if (s[i] <= '9' && s[i] >= '0'){
-            struct values *tmp = malloc(sizeof(data));
+        } else if (s[i] <= '9' && s[i] >= '0'){
+            struct values *tmp = malloc(sizeof(struct values));
             tmp->side = side;
             tmp->sign = 0;
             size_t sizeNumber = sizeNumberFloat(s + i);
@@ -191,16 +190,246 @@ struct values * parseData(char *s){
             i += sizeNumber;
             i += 5;
             sizeNumber = checkNumberInt(s + i);
-            tmpVal = malloc(sizeof(char) * (sizeNumber + 1));
-            tmpVal[sizeNumber] = '\0';
+            char *tmpVal2 = malloc(sizeof(char) * (sizeNumber + 1));
+            tmpVal2[sizeNumber] = '\0';
             for (size_t j = 0; j < sizeNumber; j++){
-                tmpVal[j] = s[i + j];
+                tmpVal2[j] = s[i + j];
             }
-            tmp->degree = atoi(tmpVal);
-            free(tmpVal);
+            tmp->degree = atoi(tmpVal2);
+            free(tmpVal2);
+            i += sizeNumber - 1;
+            if (data == NULL){
+                data = tmp;
+                data->next = NULL;
+                data->prev = NULL;
+            }else{
+                struct values *tmpData = data;
+                while (tmpData->next != NULL){
+                    tmpData = tmpData->next;
+                }
+                tmpData->next = tmp;
+                tmp->next = NULL;
+                tmp->prev = tmpData;
+            }
+        } else if (s[i] == '='){
+            struct values *tmp = malloc(sizeof(struct values));
+            tmp->side = 2;
+            side++;
+            tmp->sign = 0;
+            tmp->degree = -1;
+            char *tmp2 = strdup("=");
+            tmp->val = tmp2;
+             if (data == NULL){
+                data = tmp;
+                data->next = NULL;
+                data->prev = NULL;
+            }else{
+                struct values *tmpData = data;
+                while (tmpData->next != NULL){
+                    tmpData = tmpData->next;
+                }
+                tmpData->next = tmp;
+                tmp->next = NULL;
+                tmp->prev = tmpData;
+            }
         }
     }
-    return NULL;
+    return data;
+}
+
+struct values *multiplicationAndDivisionData(struct values *data){
+    struct values *tmp = data;
+    size_t i = 0;
+    while (tmp != NULL){
+        if (tmp->val[0] == '*'){
+            char *val = mul(tmp->prev->val, tmp->next->val);
+            int degree = tmp->prev->degree + tmp->next->degree;
+            struct values *new = malloc(sizeof(struct values));
+            new->side = tmp->side;
+            new->sign = 0;
+            new->degree = degree;
+            new->val = val;
+            if (i == 1){
+                struct values *tmp2 = tmp;
+                free(data->val);
+                free(data);
+                data = new;
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                tmp2 = tmp;
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                new->prev = NULL;
+                new->next = tmp;
+                tmp->prev = new;
+                tmp = data;
+                i = 0;
+            }else{
+                struct values *tmp2 = tmp;
+                new->prev = tmp->prev->prev;
+                new->prev->next = new;
+                free(tmp->prev->val);
+                free(tmp->prev);
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                tmp2 = tmp;
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                new->next = tmp;
+                tmp->prev = new;
+                i = 0;
+                tmp = data;
+            }
+        }else if (tmp->val[0] == '/'){
+            char *val = divi(tmp->prev->val, tmp->next->val);
+            int degree = tmp->prev->degree - tmp->next->degree;
+            struct values *new = malloc(sizeof(struct values));
+            new->side = tmp->side;
+            new->sign = 0;
+            new->degree = degree;
+            new->val = val;
+            if (i == 1){
+                struct values *tmp2 = tmp;
+                free(data->val);
+                free(data);
+                data = new;
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                tmp2 = tmp;
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                new->prev = NULL;
+                new->next = tmp;
+                tmp->prev = new;
+                tmp = data;
+                i = 0;
+            }else{
+                struct values *tmp2 = tmp;
+                new->prev = tmp->prev->prev;
+                new->prev->next = new;
+                free(tmp->prev->val);
+                free(tmp->prev);
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                tmp2 = tmp;
+                tmp = tmp->next;
+                free(tmp2->val);
+                free(tmp2);
+                new->next = tmp;
+                tmp->prev = new;
+                i = 0;
+                tmp = data;
+            }
+        }
+        tmp = tmp->next;
+        i++; 
+    }
+    return data;
+}
+
+struct values * additionAndSubtractionData(struct values *data){
+    struct values *tmp = data;
+    struct values *tmpStrut;
+    size_t i = 0;
+    while (tmp != NULL){
+        if (tmp->val[0] >= '0' && tmp->val[0] <= '9'){
+            struct values *tmp2 = tmp->next;
+            while (tmp2 != NULL){
+                if (tmp2->degree == tmp->degree && tmp->sign == tmp2->sign && tmp2->val[0] >= '0' && tmp2->val[0] <= '9'){
+                    char *valtmp = NULL;
+                    if (i == 0){
+                        valtmp = strdup(tmp->val);
+                    }else if (tmp->prev->val[0] == '-'){
+                        valtmp = join("-", tmp->val);
+                    }else{
+                        valtmp = strdup(tmp->val);
+                    }
+                    /////////// FAIRE ADDITION OU SOUSTRACTION //////////
+                    char *val = NULL;
+                    if (tmp2->prev->val[0] == '-'){
+                        if (tmp2->side == 0){
+                            val = sub(valtmp, tmp2->val);
+                        }else{
+                            val = add(valtmp, tmp2->val);
+                        }
+                    }else{
+                        if (tmp2->side == 0){
+                            val = add(valtmp, tmp2->val);
+                        }else{
+                            val = sub(valtmp, tmp2->val);
+                        }
+                    }
+                    free(valtmp);
+                    if (val[0] == '-' && i == 0){
+                        struct values *new = malloc(sizeof(struct values));
+                        new->degree = -1;
+                        new->side = 0;
+                        new->sign = 2;
+                        new->val = strdup("-");
+                        new->prev = NULL;
+                        new->next = data;
+                        data->prev = new;
+                        data = new;
+                        i++;
+                    }
+                    else if (val[0] == '-' && tmp->prev->val[0] == '='){
+                        struct values *new = malloc(sizeof(struct values));
+                        new->degree = -1;
+                        new->side = 1;
+                        new->sign = 2;
+                        new->val = strdup("-");
+                        struct values *tmp3 = tmp->prev;
+                        tmp3->next = new;
+                        new->prev = tmp3;
+                        new->next = tmp;
+                        tmp->prev = new;
+                    }else if (val[0]  == '-' && tmp->prev->val[0] == '+'){
+                        tmp->prev->val[0] = '-';
+                    }else if (val[0]  != '-' && i != 0 && tmp->prev->val[0] == '-'){
+                        tmp->prev->val[0] = '+';
+                    }
+                    free(tmp->val);
+                    if (val[0] == '-'){
+                        tmp->val = strdup(val + 1);
+                        free(val);
+                    }else{
+                        tmp->val = val;
+                    }
+                    
+                    if (tmp2->prev->val[0] == '='){
+                        struct values *tmp3 = tmp2->prev;
+                        tmp3->next = tmp2->next;
+                        if (tmp2->next != NULL) tmp2->next->prev = tmp3;
+                        free(tmp2->val);
+                        free(tmp2);
+                        tmp2 = tmp3;
+                    }else{
+                        struct values *tmp3 = tmp2->prev->prev;
+                        tmp3->next = tmp2->next;
+                        if (tmp2->next != NULL) tmp2->next->prev = tmp3;
+                        free(tmp2->prev->val);
+                        free(tmp2->prev);
+                        free(tmp2->val);
+                        free(tmp2);
+                        tmp2 = tmp3;
+                    }
+                    
+                }
+                tmp2 = tmp2->next;
+            }
+        }
+        
+        tmp = tmp->next;
+        i++;
+    }
+    return data;
 }
 
 void equation(char *s){
@@ -209,7 +438,34 @@ void equation(char *s){
         return;
     }
     struct values *data = parseData(s);
-    printf("s = %s\n", s);    
+    // creer une fonction qui fait les multiplications de chaque coter.
+    data = multiplicationAndDivisionData(data);
+    // creer une fonction qui fait les additions et soustraction de chaque coter.
+    data = additionAndSubtractionData(data);
+    // faire une fonction qui supprimme tout les 0
+    
+    // creer une fonction qui met tous les nombres du coter gauche.
+    // creer une fonction qui reduit au maximum le resultat.
+    // creer une fonction qui deduit le degrees de l'equation.
+    // si equation second degree.
+    // creer plusieur fonction en fonction du discriminant.
+    // si equation premier degree 1 solution.
+    // si equation de degrer zero toutes les solutions de x sont possible.
+    printf("s = %s\n", s);
+    struct values *tmp = data;
+    
+    while (tmp != NULL){
+        printf("%s ", tmp->val);
+        tmp = tmp->next; 
+    }
+    printf("\n");
+    tmp = data;
+    while (tmp != NULL){
+        struct values *tmp2 = tmp;
+        tmp = tmp->next;
+        free(tmp2->val);
+        free(tmp2);
+    }
 }
 
 int main(int argc, char **av) {
